0c62f1924aca37546aa1bb8cf091d605c8abc1bd
Motis <loicchandon@gmail.com>
2024-09-19 19:01:31 -0400

    ajout collisions, not working

--- player/animated_sprite_2d.gd
+++ player/animated_sprite_2d.gd
@@ -9,4 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-@onready var animated_sprite_2d: AnimatedSprite2D = $"."
--- player/sprite_2d.gd
+++ player/sprite_2d.gd
@@ -1,18 +1,18 @@
-extends Sprite2D
-
-const SPEED = 300.0
-
-func _physics_process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	var direction := Input.get_axis("left_axis","right_axis")
-	var direction2 := Input.get_axis("up_axis","down_axis")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = 0
-	if direction2:
-		velocity.y = direction2 * SPEED
-	else:
-		velocity.y = 0
-	
-	position += velocity * delta
+#extends Sprite2D
+#
+#const SPEED = 300.0
+#
+#func _physics_process(delta: float) -> void:
+	#var velocity = Vector2.ZERO
+	#var direction := Input.get_axis("left_axis","right_axis")
+	#var direction2 := Input.get_axis("up_axis","down_axis")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = 0
+	#if direction2:
+		#velocity.y = direction2 * SPEED
+	#else:
+		#velocity.y = 0
+	#
+	#position += velocity * delta
