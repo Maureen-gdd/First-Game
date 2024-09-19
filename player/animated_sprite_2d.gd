60d2cdbde86dc8c9cbdd67747d7d5df89c1eb454
Motis <loicchandon@gmail.com>
2024-09-19 18:37:41 -0400

    fin séance

--- player/animated_sprite_2d.gd
+++ player/animated_sprite_2d.gd
@@ -9,15 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-const SPEED = 300.0
-
-func _physics_process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	var direction := Input.get_axis("left_axis","right_axis")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = 0
-	
-	position += velocity * delta
+@onready var animated_sprite_2d: AnimatedSprite2D = $"."
