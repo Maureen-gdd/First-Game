221089b26ade321c69a6b8632d8ff27108e2ecb5
Motis <loicchandon@gmail.com>
2024-09-21 15:32:45 -0400

    collisions fonctionnelles

--- player/character_body_2d.gd
+++ player/character_body_2d.gd
@@ -8,15 +8,10 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	var velocity = Vector2.ZERO
-	var direction := Input.get_axis("left_axis","right_axis")
-	var direction2 := Input.get_axis("up_axis","down_axis")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = 0
-	if direction2:
-		velocity.y = direction2 * SPEED
-	else:
-		velocity.y = 0
+	var direction_x := Input.get_axis("left_axis", "right_axis")
+	var direction_y := Input.get_axis("up_axis", "down_axis")
 	
-	position += velocity * delta
+	velocity.x = direction_x * SPEED
+	velocity.y = direction_y * SPEED
+	
+	velocity = move_and_collide(delta * velocity)
